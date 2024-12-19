d35bcd28f37f09e2d5c993f308bf5e37247fb092
abdomarrok <marrokmar@gmail.com>
2024-12-19 08:11:06 +0100

    studant view shaping

--- src/main/java/com/marrok/schoolmanager/controllers/student/AddStudentController.java
+++ src/main/java/com/marrok/schoolmanager/controllers/student/AddStudentController.java
@@ -1,4 +1,16 @@
 package com.marrok.schoolmanager.controllers.student;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class AddStudentController {
+    public Button cencel_btn;
+
+    public void cencel(ActionEvent event) {
+        Stage stage =(Stage) cencel_btn.getScene().getWindow();
+        stage.close();
+
+    }
 }
